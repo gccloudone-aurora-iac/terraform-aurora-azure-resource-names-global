75b939760d254af0f40470281c07446823caf12f
Sajid Hafiz <sajid.hafiz@cloud.statcan.ca>
2025-02-10 17:47:08 -0700

    ssc naming convention

--- locals.tf
+++ locals.tf
@@ -3,13 +3,34 @@ resource "random_id" "this" {
 }
 
 locals {
-  random_number = substr(random_id.this.dec, 0, 6)
-  resource_type_abbreviations = {
+  common_convention_base_ssc = "${var.name_attributes_ssc.department_code}${var.name_attributes_ssc.environment}${var.name_attributes_ssc.csp_region}"
+  random_number              = substr(random_id.this.dec, 0, 6)
+  resource_type_abbreviations_statcan = {
     "storage account"    = "sa"
     "key vault"          = "kv"
     "container registry" = "acr"
     "postgresql server"  = "psql"
   }
+  resource_type_abbreviations_ssc = {
+    "storage account"    = "stg"
+    "key vault"          = "kv"
+    "container registry" = "registry"
+    "postgresql server"  = "psql"
+  }
 
-  resource_names = { for resource_type, abbrev in local.resource_type_abbreviations : resource_type => "${abbrev}${local.random_number}${var.user_defined}" }
+  resource_names_exception = {
+    # For any resources that does not follow the convention <dept code><environment><CSP Region>-<userDefined-string>-suffix
+    "storage account" = "${local.common_convention_base_ssc}${var.user_defined}"
+  }
+
+  resource_names_statcan = { for resource_type, abbrev in local.resource_type_abbreviations_statcan : resource_type => "${abbrev}${local.random_number}${var.user_defined}" }
+  resource_names_ssc = {
+    for resource_type, abbrev in local.resource_type_abbreviations_ssc :
+    resource_type => (
+      contains(keys(local.resource_names_exception), resource_type)
+      ? local.resource_names_exception[resource_type]
+      : "${local.common_convention_base_ssc}-${var.user_defined}-${abbrev}"
+    )
+  }
+  resource_names = var.government ? local.resource_names_ssc : local.resource_names_statcan
 }
--- variables.tf
+++ variables.tf
@@ -8,3 +8,37 @@ variable "user_defined" {
     error_message = "The user-defined field must be between 2-15 characters long."
   }
 }
+
+variable "government" {
+  type        = bool
+  default     = false
+  description = "Sets which naming convention to use. If true, use SSC's otherwise use StatCan's."
+}
+
+variable "name_attributes_ssc" {
+  type = object({
+    department_code = string
+    environment     = string
+    csp_region          = string
+  })
+
+  validation {
+    condition     = length(var.name_attributes_ssc.environment) == 1
+    error_message = "Environment must be 1 character long"
+  }
+
+  validation {
+    condition     = length(var.name_attributes_ssc.department_code) == 2
+    error_message = "Department code must be 1 character long"
+  }
+
+  validation {
+    condition     = length(var.name_attributes_ssc.region) == 1
+    error_message = "Region must be 1 character long"
+  }
+
+  validation {
+    condition = var.government
+    error_message = "The government variable is required to be true when setting the name_attributes_ssc variable."
+  }
+}
