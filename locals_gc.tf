7b270482441f0776acb1236c5b5f47cf37e87791
William Hearn <william.hearn@ssc-spc.gc.ca>
2025-04-23 21:19:38 -0400

    feat(naming): Add project to name spec

--- locals_gc.tf
+++ locals_gc.tf
@@ -14,8 +14,8 @@ locals {
 
   common_convention_base_gc = "${var.name_attributes.department_code}${var.name_attributes.environment}${local.location_gc}"
 
-  project_gc       = replace(lower(var.name_attributes.project), "-", "")
-  project_gc_short = substr(var.name_attributes.project, 0, 6)
+  project_gc       = replace(var.name_attributes.project, "-", "")
+  project_gc_short = substr(local.project_gc, 0, 5)
 
   resource_type_abbreviations_gc = {
     "azure data factory" = "adf"
@@ -26,12 +26,12 @@ locals {
   }
 
   resource_type_abbreviations_gc_custom = {
-    "data lake store"           = "${lower(local.common_convention_base_gc)}${local.project_gc}${lower(var.user_defined)}${local.random_number}"
-    "storage account"           = "${lower(local.common_convention_base_gc)}${local.project_gc}${lower(var.user_defined)}${local.random_number}"
-    "storage account container" = "${lower(local.common_convention_base_gc)}${local.project_gc}${lower(var.user_defined)}${local.random_number}-${random_string.random.result}"
-    "storage account file"      = "${lower(local.common_convention_base_gc)}${local.project_gc}${lower(var.user_defined)}${local.random_number}-${random_string.random.result}"
-    "storage account queue"     = "${lower(local.common_convention_base_gc)}${local.project_gc}${lower(var.user_defined)}${local.random_number}-${random_string.random.result}"
-    "storage account table"     = "${lower(local.common_convention_base_gc)}${local.project_gc}${lower(var.user_defined)}${local.random_number}-${random_string.random.result}"
+    "data lake store"           = "${lower(local.common_convention_base_gc)}${lower(local.project_gc)}${lower(var.user_defined)}${local.random_number}"
+    "storage account"           = "${lower(local.common_convention_base_gc)}${lower(local.project_gc)}${lower(var.user_defined)}${local.random_number}"
+    "storage account container" = "${lower(local.common_convention_base_gc)}${lower(local.project_gc)}${lower(var.user_defined)}${local.random_number}-${random_string.random.result}"
+    "storage account file"      = "${lower(local.common_convention_base_gc)}${lower(local.project_gc)}${lower(var.user_defined)}${local.random_number}-${random_string.random.result}"
+    "storage account queue"     = "${lower(local.common_convention_base_gc)}${lower(local.project_gc)}${lower(var.user_defined)}${local.random_number}-${random_string.random.result}"
+    "storage account table"     = "${lower(local.common_convention_base_gc)}${lower(local.project_gc)}${lower(var.user_defined)}${local.random_number}-${random_string.random.result}"
   }
 
   resource_names_gc = merge(
